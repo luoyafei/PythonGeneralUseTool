4a4d8ff581dc0138cb1ffbcb322de137acc8e514
yafei10 <yafei10@staff.weibo.com>
2018-10-08 12:03:44 +0800

    增加将timedelta转换为可显示的字符串

--- com/py/use_dec/use_dec.py
+++ com/py/use_dec/use_dec.py
@@ -36,12 +36,22 @@ from datetime import datetime, timedelta
 import re
 
 
-time_str = str(timedelta(seconds=3600))
+timedelta_origin = timedelta(seconds=123456)
 
-def get_time_str_by_symbol(time_str):
+def timedelta_2_format_hms(timedelta_time_str):
+    """将时间格式的字符串，转换为待显示的时间字符串
+    example:
+        input: 10:17:36
+        output: 10h17m36s
+
+    :param timedelta_time_str:
+    :type string: timedelta_time_str
+    :return:
+    """
+    print(timedelta_time_str)
     return_str = ""
     i = 0
-    for time_item in time_str.split(":"):
+    for time_item in timedelta_time_str.split(":"):
         if i == 0 and time_item != "0":
             return_str = time_item + "h"
         elif i == 1 and time_item != "00":
@@ -51,14 +61,27 @@ def get_time_str_by_symbol(time_str):
         i += 1
     return return_str
 
-def get_time(time_str):
+def timedelta_2_format_dhms(origin_time_diff):
+    """将时间差进行格式化转换
+    example:
+        input: timedelta(seconds=123456)
+        output: 1d10h17m36s
+
+    :param origin_time_diff: 时间差
+    :type origin_time_diff: datetime.timedelta
+    :return:
+    :rtype string
+    """
+    if origin_time_diff <= timedelta(0):
+        raise ValueError("'origin_time_diff=%s' cannot be less than 0" % str(origin_time_diff))
+    time_str = str(origin_time_diff)
     if time_str.__contains__("days"):
         time_str_split_day = re.split('days, ', time_str)
-        return str(int(time_str_split_day[0])) + "d" + get_time_str_by_symbol(time_str_split_day[1])
+        return str(int(time_str_split_day[0])) + "d" + timedelta_2_format_hms(time_str_split_day[1])
     elif time_str.__contains__("day"):
         time_str_split_day = re.split('day, ', time_str)
-        return str(int(time_str_split_day[0])) + "d" + get_time_str_by_symbol(time_str_split_day[1])
+        return str(int(time_str_split_day[0])) + "d" + timedelta_2_format_hms(time_str_split_day[1])
     else:
-        return get_time_str_by_symbol(time_str)
-print(time_str)
-print(get_time(time_str))
+        return timedelta_2_format_hms(time_str)
+print(timedelta_origin)
+print(timedelta_2_format_dhms(timedelta_origin))
